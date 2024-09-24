9c16ca5583369113826f2595ac6d22b38ad09278
Kaylee-Baichulall-ICD2O1-01 <kaylee.baichulall@student.tdsb.on.ca>
2024-09-24 14:14:43 +0000

    d

--- PP1_6.py
+++ PP1_6.py
@@ -26,7 +26,7 @@ def q2():
 # Output the phrase "hello Mr. Kalisz have you seen my work yet?"
 
 def q3():
-  print("\"hello Mr. Kalisz have you seen my work yet?\"")
+  print("hello Mr. Kalisz have you seen my work yet?")
 
 # From input recieve two numbers (can be decimal fractions).  
 # Output their result multiplied together.  Then round down to the nearest whole number
@@ -34,13 +34,12 @@ def q3():
 def q4():
   num1 = input("Input a number: ")
   num2 = input("Input a number: ")
-  num1 = int(num1)
-  num2 = int(num2)
+  num1 = float(num1)
+  num2 = float(num2)
+  print(int(num1 * num2))
 
-  print(num1 * num2)
-'''
+
 q1()
 q2()
 q3()
 q4()
-'''
